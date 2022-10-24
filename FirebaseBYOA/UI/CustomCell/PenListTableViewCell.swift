46e4edd12c0e95ae033854bd95be3dd9dcc2ee2c
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-10-24 17:35:20 -0600

    created UI views

---
+++ FirebaseBYOA/UI/CustomCell/PenListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PenListTableViewCell.swift
+//  FirebaseBYOA
+//
+//  Created by Esther on 10/24/22.
+//
+
+import UIKit
+
+class PenListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

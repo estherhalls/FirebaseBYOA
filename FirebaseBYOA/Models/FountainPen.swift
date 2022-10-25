894652e44ec3d5dd4afccc3d7e4217a1e6f1cc44
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-10-25 12:08:14 -0600

    added keys, failable initializer, load data function

--- FirebaseBYOA/Models/FountainPen.swift
+++ FirebaseBYOA/Models/FountainPen.swift
@@ -8,37 +8,62 @@
 import Foundation
 
 class FountainPen: Codable {
+    
+    // MARK: - Keys
+    enum Key {
+        static let collectionType = "pens"
+        static let penName = "Pen Name"
+        static let brandName = "Brand Name"
+        static let nibMaterial = "Nib Material"
+        static let nibSize = "Nib Size"
+        static let uuid = "uuid"
+        static let isFavorite = "favorite"
+    }
+    
     // MARK: - Properties
     var penName: String
     var brandName: String
     var nibMaterial: String
     var nibSize: String
     let uuid: String
-    let isFavorite: Bool
     
     // Dictionary Representation of Model Object
+    /// Used as value for child dictionary in save fountain pen function on FirebaseService file
     var fountainPenData: [String:AnyHashable] {
-        ["penName":self.penName,
-         "brandName":self.brandName,
-         "nibMaterial":self.nibMaterial,
-         "nibSize":self.nibSize,
-         "uuid":self.uuid,
-         "isFavorite":self.isFavorite
-         ]
+        [penName : self.penName,
+       brandName : self.brandName,
+     nibMaterial : self.nibMaterial,
+         nibSize : self.nibSize,
+            uuid : self.uuid
+        ]
     }
     
-    // MARK: - Initializer
-    init(penName: String, brandName: String, nibMaterial: String, nibSize: String, uuid: String = UUID().uuidString, isFavorite: Bool = false) {
+    // MARK: - Designated Initializer
+    init(penName: String, brandName: String, nibMaterial: String, nibSize: String, uuid: String = UUID().uuidString) {
         self.penName = penName
         self.brandName = brandName
         self.nibMaterial = nibMaterial
         self.nibSize = nibSize
         self.uuid = uuid
-        self.isFavorite = isFavorite
     }
     
 } // End of Class
 
+// MARK: - Optional (Failable) Initializer Extension
+///Allows us to use .compactMap for-in loop on FirebaseService
+extension FountainPen {
+    convenience init?(fromDictionary dictionary: [String:Any]) {
+        guard let penName = dictionary[Key.penName] as? String,
+              let brandName = dictionary[Key.brandName] as? String,
+              let nibMaterial = dictionary[Key.nibMaterial] as? String,
+              let nibSize = dictionary[Key.nibSize] as? String,
+              let uuid = dictionary[Key.uuid] as? String
+        else {return nil}
+        
+        self.init(penName: penName, brandName: brandName, nibMaterial: nibMaterial, nibSize: nibSize, uuid: uuid)
+    }
+}
+/// Allows us to individually identify cells to select and delete
 extension FountainPen: Equatable {
     static func == (lhs: FountainPen, rhs: FountainPen) -> Bool {
         return lhs.uuid == rhs.uuid
--- FirebaseBYOA/Networking/FirebaseService.swift
+++ FirebaseBYOA/Networking/FirebaseService.swift
@@ -8,11 +8,38 @@
 import Foundation
 import Firebase
 
+/// May be it's own file if you want to abstract further. Must be outside of FirebaseService struct
+enum FirebaseError: Error {
+    case firebaseError(Error)
+    case failedToUnwrapData
+    case noDataFound
+}
+
 struct FirebaseService {
+    
     let ref = Database.database().reference()
     
+    /// Go back to model and create keys so we don't have to hard code
     func save(fountainPen: FountainPen) {
-        ref.child("pens").updateChildValues([fountainPen.uuid : fountainPen.fountainPenData])
-        
+        /// UUID is what makes each entry unique, fountainPenData is what we named the unique dictionary representation of our model object on our model file
+        ref.child(FountainPen.Key.collectionType).updateChildValues([fountainPen.uuid : fountainPen.fountainPenData])
     }
-}
+    
+    func loadPens(completion: @escaping (Result<[FountainPen], FirebaseError>) -> Void) {
+        ref.child(FountainPen.Key.collectionType).getData { error, snapshot in
+            if let error {
+                print(error.localizedDescription)
+                completion(.failure(.firebaseError(error)))
+                return
+            }
+            guard let data = snapshot?.value as? [String: [String:Any]] else {
+                completion(.failure(.failedToUnwrapData))
+                return
+            }
+            /// Go back to model and create failable initializer so we can use .compactMap
+            let dataArray = Array(data.values)
+            let pens = dataArray.compactMap({FountainPen(fromDictionary: $0)})
+            completion(.success(pens))
+        }
+    }
+}// End of Struct
